30f4c65d9064e4b0c2ee8a32d0413c7b04933c5b
zcj <18137693952@163.com>
2023-05-30 21:20:14 +0800

    feat: 角色表定义更改

--- models/role/main.go
+++ models/role/main.go
@@ -5,6 +5,7 @@ import "simple-mall/models"
 // Role 角色表
 type Role struct {
 	models.BaseModel
-	RoleName    string `gorm:"type:varbinary(50); not null; comment '角色名称'" json:"roleName"`
+	Code        string `gorm:"type:varbinary(50); not null; comment '角色Code'" json:"code"`
+	Name        string `gorm:"type:varbinary(50); not null; comment '角色名称'" json:"name"`
 	Description string `gorm:"type:varbinary(300); not null; comment '角色描述'" json:"description"`
 }
