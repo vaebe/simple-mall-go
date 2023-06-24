4a7289f9fe6237e1a802f052f88f6d5e6987fdd8
zcj <18137693952@163.com>
2023-06-24 22:02:52 +0800

    fix(enum): slideshow model 字段 JumpLink to jumpLink

--- models/slideshow/main.go
+++ models/slideshow/main.go
@@ -6,7 +6,7 @@ import "simple-mall/models"
 type Slideshow struct {
 	models.BaseModel
 	ImageURL    string `gorm:"type:varbinary(300); not null; comment '图片地址'" json:"imageURL"`
-	JumpLink    string `gorm:"type:varbinary(300); comment '跳转链接'" json:"JumpLink"`
+	JumpLink    string `gorm:"type:varbinary(300); comment '跳转链接'" json:"jumpLink"`
 	Description string `gorm:"type:varbinary(200); comment '图片描述'" json:"description"`
 	Type        string `gorm:"type:varbinary(10); default:01; comment '图片类型 01 登录页 02 首页'" json:"type"`
 }
