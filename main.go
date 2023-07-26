25e8ae77a66826bb085ef99f414cc6e59d2bde28
zcj <18137693952@163.com>
2023-07-26 22:17:01 +0800

    feat: main.go 优化

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"simple-mall/global"
 	"simple-mall/initialize"
-	middlewares "simple-mall/middleware"
 	"simple-mall/routers"
 	"simple-mall/tasks"
 	"time"
@@ -20,24 +19,18 @@ func main() {
 	// 初始化配置
 	initialize.InitConfig()
 
-	r := gin.Default()
-	// 替换 gin logger 为 zap
-	r.Use(ginzap.Ginzap(global.Logger, time.RFC3339, true), ginzap.RecoveryWithZap(global.Logger, true))
-
-	// 添加跨域中间件
-	r.Use(middlewares.Cors())
-
-	// 添加jwt中间件
-	r.Use(middlewares.JWTAuth(routers.GetRouterWhiteList()))
-
-	// 加载路由
-	routers.LoadAllRouter(r)
-
 	// 初始化定时任务
 	tasks.InitTasks()
 
-	port := 51015
-	serviceAddress := fmt.Sprintf("%s:%d", "127.0.0.1", port)
+	r := gin.Default()
+
+	// 替换 gin logger 为 zap
+	r.Use(ginzap.Ginzap(global.Logger, time.RFC3339, true), ginzap.RecoveryWithZap(global.Logger, true))
+
+	// 初始化路由
+	routers.InitRouter(r)
+
+	serviceAddress := fmt.Sprintf("%s:%d", "127.0.0.1", 51015)
 
 	// 初始化swagger
 	initialize.InitSwagger(r, serviceAddress)
